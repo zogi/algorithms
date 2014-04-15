2e118b6489162d09b59ab35311a4e8b818424749
zogi <zoltan.gilian@gmail.com>
2014-04-15 23:02:33 +0200

    moved test code to a separate compilation unit

---
+++ min_cost_max_flow/test.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#include "min_cost_max_flow.h"
+
+using mcmf::matrix;
+using mcmf::constant_matrix;
+using mcmf::min_cost_max_match;
+
+int main()
+{
+	int na = 3, nb = 2;
+	mcmf::constant_matrix<bool> match_possible = constant_matrix<bool>(true);
+	matrix<double> cost = matrix<double>(na, nb);
+	cost(0, 0) = 11.123;
+	cost(0, 1) = 8.7;
+	cost(1, 0) = 104.7;
+	cost(1, 1) = 6.42;;
+	cost(2, 0) = 65.93;
+	cost(2, 1) = 2.42;
+	std::vector<int> matching;
+	min_cost_max_match(na, nb, match_possible, cost, matching);
+	for (int i = 0; i < na; ++i) {
+		std::cout << matching[i] << std::endl;
+	}
+	return 0;
+}
