3502cb432856c2967187ed580e4fa7dfd370c2c9
zogi <zoltan.gilian@gmail.com>
2014-04-16 17:56:57 +0200

    added linalg tests

--- linalg/test.cpp
+++ linalg/test.cpp
@@ -4,6 +4,7 @@
 #include "linalg.h"
 
 using linalg::Matrix;
+using linalg::Vector;
 using linalg::identity;
 using linalg::zeros;
 
@@ -12,7 +13,7 @@ std::ostream & operator<<(std::ostream& ost, linalg::Matrix<T, N, M> const & m)
 {
   for (int i = 0; i < m.rows; ++i) {
     for (int j = 0; j < m.cols; ++j) {
-      ost << std::setw(3) << m(i, j);
+      ost << std::setw(6) << m(i, j);
     }
     ost << std::endl;
   }
@@ -27,12 +28,17 @@ int main()
       a(i, j) = rand()%100;
 
   std::cout << a;
-  std::cout << "==========\n";
+  std::cout << "==================\n";
   std::cout << identity<int, 2>() * a;
-  std::cout << "==========\n";
+  std::cout << "==================\n";
   std::cout << a * identity<int, 3>();
-  std::cout << "==========\n";
+  std::cout << "==================\n";
   std::cout << (a == a) << "\n";
   std::cout << (a - a == zeros<int, 2, 3>()) << "\n";
+
+  auto b = Matrix<double, 2, 3>(a);
+  std::cout << a + b;
+  auto v = Vector<double, 3>(1);
+  std::cout <<  b * v;
   return 0;
 }
